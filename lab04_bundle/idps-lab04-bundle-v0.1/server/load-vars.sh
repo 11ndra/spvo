748b492e68ae58cd0ce2825e3b20ca05932eb3a1
11ndra <whzgud09@gmail.com>
2026-09-17 01:53:08 +0500

    v2.28-rc1

--- lab04_bundle/idps-lab04-bundle-v0.1/server/load-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-LAB_IFACE="$(ip -o -4 addr show | awk '$4 ~ /^10\.13\.37\.20\// {print $2; exit}')"
-if [[ -z "$LAB_IFACE" ]]; then
-  echo '[FAIL] Не найден интерфейс с 10.13.37.20/24.' >&2
-  return 1 2>/dev/null || exit 1
-fi
-export LAB_IFACE
-echo "LAB_IFACE=$LAB_IFACE"
